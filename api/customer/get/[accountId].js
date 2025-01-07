e3160fa8f4559340bd08989d37b04d682c04d43e
KoyaKal <joseph.b.d.dela.cruz@gmail.com>
2025-01-08 01:04:48 +0800

    indentions

--- api/customer/get/[accountId].js
+++ api/customer/get/[accountId].js
@@ -11,27 +11,26 @@ export default async function handler(req, res) {
   // Check if the request method is GET
   if (req.method === 'GET') {
     const accountId = req.query;
+    try {
+        // Query to get customer data based on accountId
+        const { data, error } = await supabase
+        .from('tbl_quexpress_customers')
+        .select('*')
+        .eq('account_id', accountId)
+        .single(); // Use .single() if you expect only one record
 
-        try {
-            // Query to get customer data based on accountId
-            const { data, error } = await supabase
-            .from('tbl_quexpress_customers')
-            .select('*')
-            .eq('account_id', accountId)
-            .single(); // Use .single() if you expect only one record
-
-            if (error) {
-            throw error;
-            }
-
-            res.json(data); // Send the retrieved customer data
-        } catch (err) {
-            console.error(err);
-            res.status(500).send('Server Error');
+        if (error) {
+        throw error;
         }
-    } else {
-    // Handle any other HTTP method
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+
+        res.json(data); // Send the retrieved customer data
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+  } else {
+  // Handle any other HTTP method
+  res.setHeader('Allow', ['GET']);
+  res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
