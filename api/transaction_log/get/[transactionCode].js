e98c0e8ee2fb96ad4072f91a226656ddef9231f2
KoyaKal <joseph.b.d.dela.cruz@gmail.com>
2025-01-08 00:50:38 +0800

    changing some params to query

--- api/transaction_log/get/[transactionCode].js
+++ api/transaction_log/get/[transactionCode].js
@@ -10,7 +10,7 @@ const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 export default async function handler(req, res) {
   // Check if the request method is GET
   if (req.method === 'GET') {
-    const transactionCode = req.params.transactionCode + '%';
+    const transactionCode = req.query.transactionCode + '%';
   
     try {
         const { data, error } = await supabase.rpc('get_monitor_transaction_log', { r_transaction_code: transactionCode });
