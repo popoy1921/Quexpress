cb4a073a8c0db259a358f31b1efa2fcba52c122a
John Paul dela Cruz <johnpauldelacruz@sample.com>
2025-01-08 13:01:40 +0800

    Get Advertisement

--- api/users/getAdvertisement.js
+++ api/users/getAdvertisement.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+// Initialize Supabase client
+const SUPABASE_URL = 'https://ayhfyztprntbaftgrypt.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5aGZ5enRwcm50YmFmdGdyeXB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMxMjYxNDAsImV4cCI6MjA0ODcwMjE0MH0.krmujGeIdEwo_zfrnLLMw1aWQniR-OXnxTG4ZVBPnM4'; 
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export default async (req, res) => {
   if (req.method !== 'GET') {
